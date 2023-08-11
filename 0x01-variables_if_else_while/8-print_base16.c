4fdd2e84894075594ce1b1b236dff49e0ecb4de8
Carlton <carltonokoth94@gmail.com>
2023-08-11 22:30:15 +0300

    numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - numbers of base 16
+ * Return: 0
+ */
+int main(void)
+{
+int j;
+char g;
+for (j = 0 ; j < 10 ; j++)
+putchar(j + '0');
+for (g = 'a' ; g <= 'f' ; g++)
+putchar('\n');
+return (0);
+}
