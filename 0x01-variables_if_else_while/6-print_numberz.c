20ef302fbe43ca08cee47e32fab48b27903774d5
Carlton <carltonokoth94@gmail.com>
2023-08-11 22:12:18 +0300

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - single digit numbers
+ * Return: (0)
+ */
+int main(void)
+{
+int j;
+for (j = 0 ; j < 10 ; j++)
+putchar(j + '0');
+putchar('\n');
+return (0);
+}
