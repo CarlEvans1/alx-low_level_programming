38259c158ce22d321ecde338fec0905205dafed8
Carlton <carltonokoth94@gmail.com>
2023-08-11 23:00:33 +0300

    single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+int j;
+for (j = 0 ; j < 10 ; j++)
+{
+if (j == 9)
+putchar(j + '0');
+else
+{
+putchar(j + '0');
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
+
